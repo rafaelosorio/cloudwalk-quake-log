8d7ee26216ff57c549e584107e069d2b35d06632
Rafael Osorio <rafaelosorio@gmail.com>
2024-04-06 23:52:51 -0300

    Generate a report of deaths grouped by death cause for each match

--- src/utils.ts
+++ src/utils.ts
@@ -1,73 +1,105 @@
 import axiosInstance from './service'
 
-// Interface to represent the game data
+// Interface to represent game data
 interface GameData {
   total_kills: number
   players: string[]
   kills: Record<string, number>
+  kills_by_means: Record<string, number>
 }
 
-// Function to handle the game log and return the game data
+// Function to process game log and return game data
 export async function handleLog(): Promise<Record<string, GameData>> {
   const response = await axiosInstance.get('/')
   const logContent: string = response.data
 
-  // Split the log content by line
+  // Split log content by line
   const logLines = logContent.split('\n')
 
-  // Initialize the game data object
+  // Initialize object to store game data
   const gameData: Record<string, GameData> = {}
 
-  // Initialize the current game index
+  // Initialize current game index
   let currentGameIndex = 1
 
-  // Iterate over the log lines
+  // Iterate over log lines
   logLines.forEach((line) => {
-    // Check if the line is a game start line
+    // Check if line indicates start of a new game
     if (line.includes('InitGame')) {
-      // Extract the game name
+      // Extract game name
       const gameName = `game_${currentGameIndex}`
-      // Initialize the game data object
+      // Initialize game data
       gameData[gameName] = {
         total_kills: 0,
         players: [],
-        kills: {}
+        kills: {},
+        kills_by_means: {}
       }
       currentGameIndex++
     }
 
-    // Check if the line is a kill line
+    // Check if line contains information about a kill
     if (line.includes('Kill')) {
-      // Extract the match data
-      const matchData = line.match(/Kill:\s(.*)\s/)
+      // Extract data from line
+      const matchData = line.match(
+        /Kill:\s(\d+)\s(\d+)\s(\d+):\s(.*)\skilled\s(.*)\sby\s(.*)/
+      )
 
-      // Check if the match data exists
+      // Check if data was extracted successfully
       if (matchData) {
-        // Extract the player names
-        const [, player1, player2] = matchData[1].split('killed')
+        // Extract player names and cause of death
+        const [, , , , player1Data, player2Data, causeOfDeath] = matchData
 
-        // Extract the game name
+        // Extract game name
         const gameName = `game_${currentGameIndex - 1}`
 
-        // Increment the total kills
+        // Increment total kills in game
         gameData[gameName].total_kills++
 
-        // Extract the player names
-        const players = gameData[gameName].players
-        if (!players.includes(player1.trim())) {
-          players.push(player1.trim())
+        // Extract player names
+        const player1 = player1Data.trim()
+        const player2 = player2Data.trim()
+
+        // Add players to players list if not already present
+        if (
+          !gameData[gameName].players.includes(player1) &&
+          player1 !== '<world>'
+        ) {
+          gameData[gameName].players.push(player1)
+        }
+        if (
+          !gameData[gameName].players.includes(player2) &&
+          player2 !== '<world>'
+        ) {
+          gameData[gameName].players.push(player2)
         }
 
-        // Check if the player1 is 'world'
-        if (player1.trim() === '<world>') {
-          // Extract the game name
-          const gameName = `game_${currentGameIndex - 1}`
-          // Decrement the player2 kills
-          gameData[gameName].kills[player2.trim()]--
+        // Check if player 1 was killed by <world>
+        if (player1 === '<world>') {
+          // Subtract 1 kill from player 2's score
+          if (player2 !== '<world>') {
+            gameData[gameName].kills[player2] =
+              (gameData[gameName].kills[player2] || 0) - 1
+          }
         } else {
-          // Increment the player1 kills, based on the history of the player
-          gameData[gameName].kills[player1.trim()] =
-            (gameData[gameName].kills[player1.trim()] || 0) + 1
+          // Check if player 2 was killed by <world>
+          if (player2 === '<world>') {
+            // Subtract 1 kill from player 1's score
+            gameData[gameName].kills[player1] =
+              (gameData[gameName].kills[player1] || 0) - 1
+          } else {
+            // Increment kills for player 1 if not '<world>'
+            if (player1 !== '<world>') {
+              gameData[gameName].kills[player1] =
+                (gameData[gameName].kills[player1] || 0) + 1
+            }
+          }
+        }
+
+        // Increment kills by means
+        if (causeOfDeath) {
+          gameData[gameName].kills_by_means[causeOfDeath] =
+            (gameData[gameName].kills_by_means[causeOfDeath] || 0) + 1
         }
       }
     }
